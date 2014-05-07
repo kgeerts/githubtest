e7aa3610cc556f9b98a8084d71ae8928f9f1d8e5
adminkris <adminkris@ubuntuserver.localdomain>
2014-05-07 14:04:21 +0200

    Revert "Heb een nieuwe functie toegevoegd"
    This reverts commit 2397f16e129136a6952a374444a308e8673b8198.

--- Calculator.php
+++ Calculator.php
@@ -37,30 +37,11 @@ class Calculator {
    
     }
     
-    
-     /**
-     * 
-     * 
-     * @param1 is het eerste getal dat we willen optellen
-     * @param2 is het tweede getal dat we willen optellen 
-     * @return Double Hier krijgen we de som terug
-     * 
-     * @version 1.1
-     * 
-    * @assert (1,1) == 0
-    * @assert (0,1) == -1
-    * @assert (1,0) == 1
-    * @assert (1,1) == 0
-    * @assert (1,2) == -1
-    **/
-      public function Substract($a, $b) {
+      public function Substract2($a, $b) {
         return $a - $b;
    
     }
-      public function Vermenigvuldig ($a, $b) {
-        return $a * $b;
-   
-    }
+
 }
 
 ?>
