ef8d14438ff03523597c4df1847bb6f3a1a5e4f6
adminkris <adminkris@ubuntuserver.localdomain>
2014-05-07 14:08:22 +0200

    Echo en deze na uitcheck toegevoegd

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "dit is de eerste wijziging ";
+        echo " en deze na uitcheck";
+        // put your code here
+        ?>
+    </body>
+</html>
